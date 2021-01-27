ce3cd947f11fff068c55b6ab02f9a69efe6f49a0
297**** <2973324@naver.com>
2021-01-27 14:33:22 +0000

    sdsd

--- world.cpp
+++ world.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
-
 using namespace std;
 
+
 int main(){
-	cout<<"hello world";
+	int r=9,b=7,w=55,x,y;
+	
+	for(int i=MIN;i<MAX;i++){
+		y=b*i;
+		x=w-y;
+		if(w<y)
+			return 0;
+		if(x%r==0)
+			cout<<x/r<<" "<<i<<endl;
+
+	}
+	return 0;
 }
